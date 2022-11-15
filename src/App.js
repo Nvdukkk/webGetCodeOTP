b59312f8402173b74ee4886a50b914750e0f21de
Nvdukkk <103177727+Nvdukkk@users.noreply.github.com>
2022-11-15 19:40:33 +0700

    fix Không isLogin thì không vào được register

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// Không isLogin thì không vào được register
 
 import { useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
@@ -9,9 +8,31 @@ function App() {
   const navigate = useNavigate();
   const { pathname } = useLocation();
   const isLogin = JSON.parse(localStorage.getItem("isLogin"));
-
-  useEffect(() => {
-    if (!isLogin) navigate("/Login");
+  // || "/Request" || "/Dashboard" || "/History" || "/Profile" || "/Password"
+  useEffect(() => { 
+    if (!isLogin) {
+      if (pathname === "/Billing") {
+        navigate("/Login")
+      }
+      if (pathname === "/Request") {
+        navigate("/Login")
+      }
+      if (pathname === "/Dashboard") {
+        navigate("/Login")
+      }
+      if (pathname === "/History") {
+        navigate("/Login")
+      }
+      if (pathname === "/Profile") {
+        navigate("/Login")
+      }
+      if (pathname === "/Password") {
+        navigate("/Login")
+      }
+      if (pathname === "/") {
+        navigate("/Login")
+      }
+    } 
     if (pathname === "/Login" && isLogin) localStorage.removeItem("isLogin");
   }, [pathname]);
 
